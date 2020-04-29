593767c3e39fbd597c1bff6bd0c4e0a990e7a18b
Leticia <lsf@protonmail.ch>
2020-04-29 17:45:10 +0200

    menu apre ma non chiude

--- ex-hamburger-menu-base/main.js
+++ ex-hamburger-menu-base/main.js
@@ -1,6 +1,3 @@
 // L'esercitazione di oggi prevede di aggiungere il codice js necessario per visualizzare l'hamburger menu su mobile, a partire da html e css già pronti.
-
-
-$(".fa-bars").click(function(){
-  $(".hamburger-menu").toggle('show');
-});
+// $(.hamburger-menu).alert('ciao')
+alert('ciao')
